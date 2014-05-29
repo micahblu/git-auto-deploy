96c3251f035399934c53d9dfed5cc7f0a20cdf4d
Micah Blu <micahblu@gmail.com>
2014-05-28 22:51:17 -0400

    Initial commit

---
+++ deploy.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Git Auto Deploy
+ * Automatically Deploy your Github Projects
+ * 
+ * @author  micahblu <github.com/micahblu>
+ * @version  0.1
+ * 
+ */
+
+error_reporting(true);
+ini_set('display_errors', 1);
+
+define('SECRET', 'Kratos22!');
+
+if(!$_REQUEST['payload']) {
+	die('Bad request: No payload');
+}
+
+$repoMap = array(
+	"wp-foundation" => "/var/www/vhosts/default/subdomains/dev/wp-foundation/wp-content/themes/wp-foundation/"
+);
+
+// If as secret is set, compare hashes
+if( defined( SECRET ) ){
+	$body = file_get_contents('php://input');
+
+	$localSignature = hash_hmac('sha1', $body, $secret);
+
+	$remoteSignature = str_replace("sha1=", "", $_SERVER['HTTP_X_HUB_SIGNATURE']);
+
+	if( $remoteSignature !== $localSignature ){
+		die('Bad request');
+	}
+}
+
+$payload = json_decode($_REQUEST['payload']);
+
+chdir( $repoMap[$payload->repository->name] );
+
+exec( 'whoami; pwd; git pull 2>&1', $output );
+
+$message = '';
+foreach($line as $output){
+	$message .=  $line . "\n";
+}
+
+mail("micahblu@gmail.com", $payload->repository->name . " deployed", $message);
+print_r($output);
